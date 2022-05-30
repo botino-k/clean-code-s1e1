d1bc0dd56ee07fe41a4893958ddc4d5fe4d04f93
botino-k <lassmary@bk.ru>
2022-05-31 02:58:24 +0300

    refactor: change tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.querySelector(".todo-block");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".complete-block");//completed-tasks
 
 
 //New task list item
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("check-block__item_edit");
+    var containsClass=listItem.classList.contains("todo-block__item_edit");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("check-block__item_edit");
+    listItem.classList.toggle("todo-block__item_edit");
 };
 
 
